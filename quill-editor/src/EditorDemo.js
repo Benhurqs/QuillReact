fc3aaeaf73fc8f9be6d6fce870aaf8e15b338f81
Benhur Quintino de Souza <benhur@zws-benhur-souza.zoom.local>
2019-07-02 13:19:36 -0300

    Criando um botao customizado

--- quill-editor/src/EditorDemo.js
+++ quill-editor/src/EditorDemo.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Editor} from "primereact/editor";
 import {Button} from "primereact/button";
+import {add} from "./utils.js"
 
 import katex from 'katex';
 import 'katex/dist/katex.min.css';
@@ -14,9 +15,22 @@ export class EditorDemo extends Component {
             text1 : '<span class="ql-formats"><button class="ql-formula" aria-label="Formula" type="button">Edit<button></span>',
             text2 : '<b onclick={console.log("cliquei")}><span class="ql-formula" data-value="c = \\pm\\sqrt{a^2 + b^3}"/></b>'
         };
+
+            // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+
+        console.log('The link was clicked.');
+        this.setState(state => ({
+            text2:'<b>oi</b>'
+        }));
     }
 
     renderHeader() {
+
+    
         return (
             <span className="ql-formats">
                 <button className="ql-bold" aria-label="Bold"></button>
@@ -24,11 +38,25 @@ export class EditorDemo extends Component {
                 <button className="ql-underline" aria-label="Underline"></button>
                 <button className="ql-formula" aria-label="Formula"></button>
                 <button className="ql-link" aria-label="Link"></button>
+                <button id="custom_test" onClick={this.handleClick}>Testando</button>
             </span>
         );
     }
 
+    // customTestButton(){
+    //     var customButton = document.querySelector('#custom-button');
+    //     customButton.addEventListener('click', function() {
+    //          console.log('Clicked!');
+    //     });
+    //     return customButton;
+    // }   
+
     render() {
+
+
+        console.log(add(2,3));
+
+
         const header = this.renderHeader();
         console.log(this.state.text2)
 
@@ -46,7 +74,11 @@ export class EditorDemo extends Component {
 
                 <div className="content-section implementation">
                     <h3 className="first">Custom Toolbar</h3>
-                    <Editor headerTemplate={header} style={{height:'320px'}} value={value} onTextChange={(e)=>this.setState({text2:e.htmlValue})}/>
+                    <Editor 
+                        headerTemplate={header} 
+                        style={{height:'320px'}} 
+                        value={value} 
+                        onTextChange={(e)=>this.setState({text2:e.htmlValue})}/>
                     <p>Value: {this.state.text2 ||'empty'}</p>
                     <Button label="Clear" icon="pi pi-times" onClick={() => this.setState({text2:''})}/>
                 </div>
---
+++ quill-editor/src/utils.js
@@ -0,0 +1,13 @@
+export function add(x, y) {
+  return x + y
+}
+
+
+   
+
+// export customButton(){
+// 	var customButton = document.querySelector('#custom-button');
+//     customButton.addEventListener('click', function() {
+//          console.log('Clicked!');
+//     });
+// }      
